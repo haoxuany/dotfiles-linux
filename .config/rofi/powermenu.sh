104e76eb3290818bad7f019c961528498cc157c7
Haoxuan (Aaron) Yue <yuehaoxuanar@yahoo.com.sg>
2025-05-23 01:37:19 -0700

    powermenu tweaks

--- .config/rofi/powermenu.sh
+++ .config/rofi/powermenu.sh
@@ -1,89 +1,39 @@
 #!/usr/bin/env bash
 
 # Options
-hibernate='󱣻'
 shutdown='⏻'
 reboot='󰜉'
 lock=''
 suspend=''
 logout='󰍃'
-yes=''
-no=''
 
-# Rofi CMD
 rofi_cmd() {
 	rofi -dmenu \
 		-hover-select -me-select-entry '' -me-accept-entry MousePrimary \
-		-p "Power" \
 		-theme $HOME/.config/rofi/powermenu.rasi
 }
 
-# Confirmation CMD
-confirm_cmd() {
-	rofi -theme-str 'window {location: center; anchor: center; fullscreen: false; width: 350px;}' \
-		-theme-str 'mainbox {orientation: vertical; children: [ "message", "listview" ];}' \
-		-theme-str 'listview {columns: 2; lines: 1;}' \
-		-theme-str 'element-text {horizontal-align: 0.5;}' \
-		-theme-str 'textbox {horizontal-align: 0.5;}' \
-		-dmenu \
-		-hover-select -me-select-entry '' -me-accept-entry MousePrimary \
-		-p 'Confirmation' \
-		-mesg 'Are you Sure?' \
-		-theme $HOME/.config/rofi/powermenu.rasi
-}
-
-# Ask for confirmation
-confirm_exit() {
-	echo -e "$yes\n$no" | confirm_cmd
-}
-
-# Pass variables to rofi dmenu
 run_rofi() {
-	echo -e "$lock\n$suspend\n$logout\n$hibernate\n$reboot\n$shutdown" | rofi_cmd
+	echo -e "$lock\n$suspend\n$logout\n$reboot\n$shutdown" | rofi_cmd
 }
 
-# Execute Command
-run_cmd() {
-	# selected="$(confirm_exit)"
-	selected="$yes"
-	if [[ "$selected" == "$yes" ]]; then
-		if [[ $1 == '--shutdown' ]]; then
-			systemctl poweroff
-		elif [[ $1 == '--reboot' ]]; then
-			systemctl reboot
-		elif [[ $1 == '--hibernate' ]]; then
-			systemctl hibernate
-		elif [[ $1 == '--suspend' ]]; then
-			mpc -q pause
-			amixer set Master mute
-			systemctl suspend
-		elif [[ $1 == '--logout' ]]; then
-			hyprlock
-		fi
-	else
-		exit 0
-	fi
-}
-
-# Actions
 chosen="$(run_rofi)"
 case ${chosen} in
     $shutdown)
-		run_cmd --shutdown
+		systemctl poweroff
         ;;
     $reboot)
-		run_cmd --reboot
-        ;;
-    $hibernate)
-		run_cmd --hibernate
+		systemctl reboot
         ;;
     $lock)
 		hyprlock
         ;;
     $suspend)
-		run_cmd --suspend
+		mpc -q pause
+		amixer set Master mute
+		systemctl suspend
         ;;
     $logout)
-		run_cmd --logout
+		hyprlock
         ;;
 esac
